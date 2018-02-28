d917549d8867be0f62439e8ca2fbf1f1500cf60c
郝亮 <haol@fenqi.im>
2018-02-28 17:50:35 +0800

    使用module

---
+++ grammar/module.py
@@ -0,0 +1,7 @@
+def hello():
+    print('Hello World!')
+
+
+__version__ = '0.0.1'
+
+print(__version__)
---
+++ grammar/using_name.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print('runing by itself')
+else:
+    print('running by another module')
